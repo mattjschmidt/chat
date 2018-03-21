3618495c1ee6e4272d552416797eb28d6ca1c6c1
Matthew Schmidt <mjschmidt@admnet.oakland.edu>
2018-03-21 16:35:59 -0400

    Still fixing styling

--- styling/chat.js
+++ styling/chat.js
@@ -15,8 +15,10 @@ $(function () {
   });
   $('form').submit(function(){
     var messageField = document.getElementById('message').value;
+	var now = Date.now();
     var chatObj = {message: messageField};
-    $('#messages').append($('<li class="sent">').text("(" + chatObj.time + ") " + username + ": " + chatObj.message));
+    $('#messages').append($('<div class="sent" id=' + username + now + '>'));
+	$('#' + username + now).append($('<p class="test">')).text(username + ": " + chatObj.message);
     socket.emit('chat message', chatObj);
     $('#message').val('');
     return false;
@@ -24,7 +26,8 @@ $(function () {
   socket.on('chat message', function(chatObj){
     if(chatObj.id != socket.id){
       play();
-      $('#messages').append($('<li class="recieved">').text("(" + chatObj.time + ") " + chatObj.username + ": " + chatObj.message));
+      $('#messages').append($('<div class="received" id=' + chatObj.username + chatObj.time + '>').text(chatObj.username + ": " + chatObj.message));
+	  $('#' + chatObj.username + chatObj.time).text(chatObj.username + ": " + chatObj.message);
       window.scrollTo(0, document.body.scrollHeight);
     }
   });
@@ -39,10 +42,10 @@ $(function () {
   });
   socket.on('user connected', function(user){
     username = user;
-    $('#messages').append($('<li style="text-align: center;">').text(user + " has joined the chat."));
+    $('#messages').append($('<div class="welcome" style="text-align: center;">').text(user + " has joined the chat."));
   });
   socket.on('user disconnected', function(username){
-    $('#messages').append($('<li style="text-align: center;">').text(username + " has left."));
+    $('#messages').append($('<div class="welcome" style="text-align: center;">').text(username + " has left."));
   });
 });
 
